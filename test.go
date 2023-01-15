1956b90dd7fc9a4a70ac437c80fe4a42fa7477ea
“Achestnut” <2307537911@qq.com>
2023-01-15 12:14:51 +0800

    guessNumber

--- test.go
+++ test.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World! This is Achestnut!!!")
+	maxNum := 100
+	rand.Seed(time.Now().UnixNano())
+	secretNumber := rand.Intn(maxNum)
+	//fmt.Println("The secret number is ", secretNumber)
+
+	fmt.Println("Please input your guess")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Println("An error occured while reading input. Please try again", err)
+			continue
+		}
+		input = strings.TrimSuffix(input, "\r\n")
+
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter on integer value")
+			continue
+		}
+		fmt.Println("You guess is ", guess)
+		if guess > secretNumber {
+			fmt.Println("Your guess is bigger then the secret number. Please try again")
+		} else if guess < secretNumber {
+			fmt.Println("Your guess is smaller than the secret number. Please try again")
+		} else {
+			fmt.Println("Correct, you legend!")
+			break
+		}
+	}
+
 }
